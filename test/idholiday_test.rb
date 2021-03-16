71163c213f5cc6a49309ee5b8da995deec41c003
irfan <tsubaisostaff@irfans-MacBook-Pro.local>
2021-03-16 21:43:17 +0900

    add test

--- test/idholiday_test.rb
+++ test/idholiday_test.rb
@@ -11,10 +11,34 @@ class IdHolidayTest <  Test::Unit::TestCase
 
     assert_equal 2020, IdHoliday.new(2020, 2).year
     assert_equal 2, IdHoliday.new(2020, 2).month
+  end
 
+  test 'list' do
     assert_equal 1, IdHoliday.new(2021, 2).list.size
     assert_equal Date.new(2021, 2, 12), IdHoliday.new(2021, 2).list[0].date
     assert_equal "Tahun Baru Imlek", IdHoliday.new(2021, 2).list[0].name
     assert IdHoliday.new(2021, 2).list[0].confirmed
+  end
+
+  test 'check' do
+    assert IdHoliday.check(Date.new(2020, 1, 1))
+    assert IdHoliday.check(Date.new(2020, 5, 10))
+    assert_not IdHoliday.check(Date.new(2020, 1, 23))
+  end
+
+  test 'name' do
+    assert_equal 'Tahun Baru Masehi', IdHoliday.name(Date.new(2020, 1, 1))
+    assert_equal 'Independent Day', IdHoliday.name(Date.new(2020, 7, 17))
+    assert_nil IdHoliday.name(Date.new(2020, 7, 14)) # Not holiday
+  end
+
+  test 'between' do
+    assert_equal 13, IdHoliday.between(Date.new(2020, 1, 1), Date.new(2020, 5, 31)).size
+    assert_raise(ArgumentError) {
+      IdHoliday.between(nil, Date.new(2020, 5, 31))
+      IdHoliday.between(Date.new(2020, 5, 31), nil)
+      IdHoliday.between(nil, nil)
+      IdHoliday.between(Date.new(2021, 5, 31), Date.new(2020, 5, 31))
+    }
   end
 end
